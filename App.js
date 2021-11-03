35d548e0c5d9ea3c6bb25ff94602c052f181465c
Yell0wF1sh <86252839+Yell0wF1sh@users.noreply.github.com>
2021-11-03 18:10:21 -0400

    18:10 11/03/21

--- App.js
+++ App.js
@@ -1,21 +1,95 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, FlatList, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [userId, setUserId] = useState("")
+  const [isNewUser, setIsNewUser] = useState(false)
+  const [repos, setRepos] = useState(["None"])
+  const [showReps, setShowReps] = useState(false)
+
+  useEffect(() => {
+    fetch('http://api.github.com/users/'+userId+'/repos')
+    // fetch('http://cs.brandeis.edu/~tim/timgithub.json')
+      .then((response) => response.json())
+      .then((reposdata) => {
+          setRepos(getName(reposdata))
+          console.log(repos)
+      })
+      .catch((error) => console.error(error))
+  },[showReps])
+
+  const renderRepos = ({item}) => {
+    <View style={{backgroundColor: 'lightgrey', padding: 10}}>
+      <Text style={{fontSize: 32}}>{item}</Text>
+    </View>
+  }
+  
+
+  let button = (
+    <TouchableOpacity
+      onPress={() => {setShowReps(!showReps)}}
+    >
+      <Text style={{color: 'blue'}}>show repositoires</Text>
+    </TouchableOpacity>
+  )
+
+  if (showReps) {
+    button =
+    <TouchableOpacity
+      onPress={() => {
+        setShowReps(!showReps)
+        setRepos(["None"])
+      }}
+    >
+      <Text style={{color: 'blue'}}>hide repositoires</Text>
+    </TouchableOpacity>
+  }
+
+  function getName(item) {
+    let array = []
+    for (let x = 0; x < item.length; x++) {
+      array.push(item[x].name)
+    }
+    return array
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={{color: 'red', fontSize: 40, textAlign: 'center'}}>Github Viewer</Text>
+      </View>
+      <View style={{ flexDirection:'row'}}>
+        <Text>github id:</Text>
+        <TextInput 
+          placeholder='userid'
+          onChangeText={(text) => {
+            setUserId(text)
+            setIsNewUser(true)
+          }}
+        />
+      </View>
+      {button}
+      <View style={styles.flatlist}>
+        <FlatList
+          data={repos}
+          renderItem={renderRepos}
+        />
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'black',
+    paddingVertical: 20
+  }, 
+  flatlist: {
+    padding: 10,
+  },  
 });
