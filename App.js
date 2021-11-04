70358c6e5c4650d7d2fba8a93ebaf04fb37bb7c1
Yell0wF1sh <86252839+Yell0wF1sh@users.noreply.github.com>
2021-11-03 20:55:23 -0400

    20:55 11/3/21

--- App.js
+++ App.js
@@ -1,55 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, FlatList, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList, TextInput, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
 
 export default function App() {
   const [userId, setUserId] = useState("")
   const [isNewUser, setIsNewUser] = useState(false)
-  const [repos, setRepos] = useState(["None"])
+  const [repos, setRepos] = useState([])
   const [showReps, setShowReps] = useState(false)
 
   useEffect(() => {
-    fetch('http://api.github.com/users/'+userId+'/repos')
-    // fetch('http://cs.brandeis.edu/~tim/timgithub.json')
+    fetch('http://api.github.com/users/' + userId + '/repos')
+      // fetch('http://cs.brandeis.edu/~tim/timgithub.json')
       .then((response) => response.json())
       .then((reposdata) => {
-          setRepos(getName(reposdata))
-          console.log(repos)
+        setRepos(getName(reposdata))
+        console.log(repos)
       })
       .catch((error) => console.error(error))
-  },[showReps])
+  }, [showReps])
 
-  const renderRepos = ({item}) => {
-    <View style={{backgroundColor: 'lightgrey', padding: 10}}>
-      <Text style={{fontSize: 32}}>{item}</Text>
-    </View>
+  const renderRepos = ({ item }) => {
+    return (
+      <View style={{ padding: 10 }}>
+        <View style={{ backgroundColor: 'lightgrey', padding: 10 }}><Text style={{ fontSize: 24 }}>{item.name}</Text></View>
+      </View>
+    )
   }
-  
+
+  let flatlist = (
+    <View style={{ padding: 10 }}>
+      <View style={{ backgroundColor: 'lightgrey', padding: 10 }}><Text style={{ fontSize: 24 }}>None</Text></View>
+    </View>
+  )
 
   let button = (
     <TouchableOpacity
-      onPress={() => {setShowReps(!showReps)}}
+      onPress={() => { setShowReps(!showReps) }}
     >
-      <Text style={{color: 'blue'}}>show repositoires</Text>
+      <Text style={{ fontSize: 20, color: 'blue' }}>show repositoires</Text>
     </TouchableOpacity>
   )
 
   if (showReps) {
     button =
-    <TouchableOpacity
-      onPress={() => {
-        setShowReps(!showReps)
-        setRepos(["None"])
-      }}
-    >
-      <Text style={{color: 'blue'}}>hide repositoires</Text>
-    </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          setShowReps(!showReps)
+        }}
+      >
+        <Text style={{ fontSize: 20, color: 'blue' }}>hide repositoires</Text>
+      </TouchableOpacity>
+
+    flatlist =
+      <FlatList
+        data={repos}
+        renderItem={renderRepos}
+      />
   }
 
   function getName(item) {
     let array = []
     for (let x = 0; x < item.length; x++) {
-      array.push(item[x].name)
+      array.push({ "name": item[x].name })
     }
     return array
   }
@@ -57,11 +68,12 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={{color: 'red', fontSize: 40, textAlign: 'center'}}>Github Viewer</Text>
+        <Text style={{ color: 'red', fontSize: 25, textAlign: 'center', fontWeight: '600' }}>Github Viewer</Text>
       </View>
-      <View style={{ flexDirection:'row'}}>
-        <Text>github id:</Text>
-        <TextInput 
+      <View style={{ flexDirection: 'row' }}>
+        <Text style={{ fontSize: 25, fontWeight: '600' }}>github id: </Text>
+        <TextInput
+          style={{ fontSize: 25, fontWeight: '600', width: "50%" }}
           placeholder='userid'
           onChangeText={(text) => {
             setUserId(text)
@@ -70,26 +82,31 @@ export default function App() {
         />
       </View>
       {button}
-      <View style={styles.flatlist}>
-        <FlatList
-          data={repos}
-          renderItem={renderRepos}
-        />
+      <View style={styles.list}>
+        {flatlist}
       </View>
-
+      <View>
+        <Text>DEBUGGING</Text>
+        <Text>userId: {userId}</Text>
+        <Text>showRepos: {showReps.toString()}</Text>
+        <Text>repos.length: {repos.length}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '#fff',
   },
   header: {
     backgroundColor: 'black',
-    paddingVertical: 20
-  }, 
-  flatlist: {
-    padding: 10,
-  },  
+    paddingVertical: 20,
+  },
+  list: {
+    alignItems: 'flex-start',
+    height: 430,
+    padding: 5,
+  },
 });
